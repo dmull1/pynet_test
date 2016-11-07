a1b557679b96573dcdcc5067045a594d09576abe
Dennis Mull <dmull1@its.jnj.com>
2016-11-07 08:29:27 -0800

    adding str_ex1.py

---
+++ str_ex1.py
@@ -0,0 +1,12 @@
+name1 = "Colonel Sanders"
+name2 = "Auntie Anne"
+name3 = "Famous Amos"
+name4 = raw_input("Enter another name: ")
+
+print
+print "{:>30}".format(name1)
+print "{:>30}".format(name2)
+print "{:>30}".format(name3)
+print "{:>30}".format(name4)
+print
+
