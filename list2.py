6f1240c40c247574cee18009700fa72999b072a2
dmull1 <dmull1@its.jnj.com>
2016-11-07 14:46:53 -0500

    Update list2.py

--- list2.py
+++ list2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-my_list = range(5)
+my_list = range(7)
 my_list.append('something')
 my_list.append('nothing')
-print my_list.pop(0)
+print my_list.pop(3)
 
 print "Length of list: {}".format(len(my_list))
 my_list.sort()
