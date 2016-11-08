542e57be71297159fd664c114c8e5fb404b05298
dmull1 <dmull1@its.jnj.com>
2016-11-08 16:17:39 -0500

    Create nx1.py

---
+++ nx1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from pprint import pprint
+from getpass import getpass
+from pynxos.device import Device
+
+nexus_ip = "nxos1.twb-tech.com"
+nxs_test = Device(host=nexus_ip, username="pyclass", password=getpass(),
+                  transport='https', port=8443)
+
+my_facts = nxs_test.facts
+pprint(nxs_test.facts)
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security
